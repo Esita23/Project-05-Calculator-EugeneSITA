706a1351939ed8221129076971806011cf9c24b9
Eugene SITA <135968759+Esita23@users.noreply.github.com>
2023-08-15 23:53:58 +0100

    add var global

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 { calculate } import './calculator';
+import{calculate}from "./calculator.js"
 
 // TODO: Faire la manipulation du DOM dans ce fichier
+function calculator(m,n){
+    const buttons = [];
+    const Nums= [];
+    let addition= n+m;
+    let product= n*m;
+    let soustraction= n-m;
+    let pourcentage = n%m;
+    let division = n/m;
+    
+}
+
+function Affiche(visbility){
+const input=document.createElement("button");
+const calcul=document.createElement("input");
+}
+function AssignButtons(){
+    document.getElementsByClassName('buttons').innerHTML='button'
+
+}
